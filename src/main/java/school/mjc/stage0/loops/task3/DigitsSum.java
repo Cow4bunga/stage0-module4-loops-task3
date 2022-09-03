b931c934752abc1fc370bb58c117b4d6f1b08a8e
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-09-04 00:10:19 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+        String n= String.valueOf(Math.abs(t));
+        int sum=0;
+        for(int i=0;i<n.length();i++){
+            int tmp=Integer.parseInt(String.valueOf(n.charAt(i)));
+            sum+=tmp;
+        }
 
+        System.out.println(sum);
     }
+
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
+        int num=0;
+        int sum=0;
+        for (int i=1;i<=lengthOfLastNumber;i++){
+            num+=9;
+            sum+=num;
+            num*=10;
+        }
 
+        System.out.println(sum);
     }
 }
